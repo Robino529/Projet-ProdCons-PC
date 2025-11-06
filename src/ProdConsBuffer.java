6718bb9fe4aaa6a9537d033828090d2489c622b6
Robin Courault <robin.courault@etu.univ-grenoble-alpes.fr>
2025-11-06 10:19:52 +0100

    Initial commit - setup project

---
+++ src/ProdConsBuffer.java
@@ -0,0 +1,19 @@
+public class ProdConsBuffer implements IProdConsBuffer {
+	public ProdConsBuffer() {
+	}
+
+	@Override
+	public void put(Message m) throws InterruptedException {
+
+	}
+
+	@Override
+	public int nmsg() {
+		return 0;
+	}
+
+	@Override
+	public int totmsg() {
+		return 0;
+	}
+}
